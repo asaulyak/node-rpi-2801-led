cc74ad82a8739d175e2e729724f3e5c015805494
jenya.asaulyak <jenya.asaulyak@gmail.com>
2016-12-24 23:34:06 +0200

    Improve rainbow module

--- server.js
+++ server.js
@@ -3,6 +3,4 @@ var renderer = require('./renderer');
 
 var PIXELS_COUNT = parseInt(process.argv[2], 10) || 10;
 
-var pixels = new Array(PIXELS_COUNT).fill(0);
-
 renderer.run(PIXELS_COUNT, animation.requestFrame);
